4e3741aba30734248ac59f301510bd629c6bdaab
SusmoySenGupta <susmoy.cse@gmail.com>
2023-03-19 22:12:02 +0600

    create Auth API and Slice

--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -1,7 +1,67 @@
 import { apiSlice } from '../api/apiSlice';
+import authSlice from './authSlice';
 
 export const authApi = apiSlice.injectEndpoints({
 	endpoints: (builder) => ({
-		// auth endpoints here...
+		register: builder.mutation({
+			query: (newUser) => ({
+				url: '/register',
+				method: 'POST',
+				body: newUser,
+			}),
+			async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+				try {
+					const { data } = await queryFulfilled;
+
+					localStorage.setItem(
+						'auth',
+						JSON.stringify({
+							accessToken: data.accessToken,
+							user: data.user,
+						})
+					);
+
+					dispatch(
+						authSlice.actions.userLoggedIn({
+							accessToken: data.accessToken,
+							user: data.user,
+						})
+					);
+				} catch (err) {
+					console.error(err);
+				}
+			},
+		}),
+		login: builder.mutation({
+			query: (newUser) => ({
+				url: '/login',
+				method: 'POST',
+				body: newUser,
+			}),
+			async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+				try {
+					const { data } = await queryFulfilled;
+
+					localStorage.setItem(
+						'auth',
+						JSON.stringify({
+							accessToken: data.accessToken,
+							user: data.user,
+						})
+					);
+
+					dispatch(
+						authSlice.actions.userLoggedIn({
+							accessToken: data.accessToken,
+							user: data.user,
+						})
+					);
+				} catch (err) {
+					console.error(err);
+				}
+			},
+		}),
 	}),
 });
+
+export const { useRegisterMutation, useLoginMutation } = authApi;
