e8a0e9c840046200e9d0b9d22e0617f4b41539cd
SusmoySenGupta <susmoy.cse@gmail.com>
2023-03-19 23:20:11 +0600

    feat: add login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,37 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import logoImage from '../assets/images/s-logo.png';
 import Error from '../components/ui/Error';
+import { useLoginMutation } from '../features/auth/authApi';
+
 export default function Login() {
+	const [login, { data, isLoading, error: responseError }] =
+		useLoginMutation();
+	const navigate = useNavigate();
+
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setError('');
+
+		login({
+			email,
+			password,
+		});
+	};
+
+	useEffect(() => {
+		if (responseError?.data) {
+			setError(responseError.data);
+		}
+		if (data?.accessToken && data?.user) {
+			navigate('/inbox');
+		}
+	}, [data, responseError, navigate]);
+
 	return (
 		<div className="grid place-items-center h-screen bg-[#F9FAFB">
 			<div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -18,8 +48,11 @@ export default function Login() {
 							Sign in to your account
 						</h2>
 					</div>
-					<form className="mt-8 space-y-6" action="#" method="POST">
-						<input type="hidden" name="remember" value="true" />
+					<form
+						className="mt-8 space-y-6"
+						method="POST"
+						onSubmit={handleSubmit}
+					>
 						<div className="rounded-md shadow-sm -space-y-px">
 							<div>
 								<label
@@ -36,6 +69,8 @@ export default function Login() {
 									required
 									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
 									placeholder="Email address"
+									value={email}
+									onChange={(e) => setEmail(e.target.value)}
 								/>
 							</div>
 							<div>
@@ -50,6 +85,10 @@ export default function Login() {
 									required
 									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
 									placeholder="Password"
+									value={password}
+									onChange={(e) =>
+										setPassword(e.target.value)
+									}
 								/>
 							</div>
 						</div>
@@ -67,6 +106,7 @@ export default function Login() {
 
 						<div>
 							<button
+								disabled={isLoading}
 								type="submit"
 								className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
 							>
@@ -74,7 +114,7 @@ export default function Login() {
 							</button>
 						</div>
 
-						<Error message="There was an error" />
+						{error !== '' && <Error message={error} />}
 					</form>
 				</div>
 			</div>
