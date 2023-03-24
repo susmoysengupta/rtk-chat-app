03edcb494926d53c63a10af49784ff84f90beda7
SusmoySenGupta <susmoy.cse@gmail.com>
2023-03-24 22:18:43 +0600

    feat: add logout when jwt is expired

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,17 +1,27 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { userLoggedOut } from '../auth/authSlice';
+
+const baseQuery = fetchBaseQuery({
+	baseUrl: process.env.REACT_APP_API_URL,
+	prepareHeaders: async (headers, { getState }) => {
+		const token = getState()?.auth?.accessToken;
+		if (token) {
+			headers.set('Authorization', `Bearer ${token}`);
+		}
+		return headers;
+	},
+});
 
 export const apiSlice = createApi({
 	reducerPath: 'api',
-	baseQuery: fetchBaseQuery({
-		baseUrl: process.env.REACT_APP_API_URL,
-		prepareHeaders: async (headers, { getState }) => {
-			const token = getState()?.auth?.accessToken;
-			if (token) {
-				headers.set('Authorization', `Bearer ${token}`);
-			}
-			return headers;
-		},
-	}),
+	baseQuery: async (args, api, extraOptions) => {
+		let response = await baseQuery(args, api, extraOptions);
+		if (response?.error?.status === 401) {
+			api.dispatch(userLoggedOut());
+			localStorage.clear('auth');
+		}
+		return response;
+	},
 	tagTypes: [],
 	endpoints: (builder) => ({}),
 });
